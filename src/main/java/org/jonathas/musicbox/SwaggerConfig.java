cbe986e1308c90c80968c141a2039b96093c3a72
CGI_jonathas <jonathas.mendesalves@cgi.com>
2019-12-17 15:30:24 -0500

    Swagger

---
+++ src/main/java/org/jonathas/musicbox/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package org.jonathas.musicbox;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build().apiInfo(apiEndPointsInfo());
+    }
+    private ApiInfo apiEndPointsInfo() {
+        return new ApiInfoBuilder().title("MusicBox. We music your life.")
+                .description("REST API to List Jukeboxes that support a giving setting")
+                .contact(new Contact("Jonathas Alves", "https://github.com/gordonbr/musicbox", "jonathasalves20@gmail.com"))
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .version("1.0.0")
+                .build();
+    }
+}
